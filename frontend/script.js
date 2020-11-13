7b2a8851b038828943a6849dc29013568ef271fe
Ubuntu <ChangerOfWays@gmail.com>
2020-11-13 19:08:39 +0000

    Added ReactJS and a background CSS gradient.

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { liked: false };
+	}
+
+	render() {
+		if( this.state.liked ) {
+			return 'You liked this.';
+		}
+		return e(
+			'button',
+			{ onClick: () => this.setState({liked:true}) },
+			'Like'
+		);
+	}
+}
+
 document.addEventListener( "DOMContentLoaded", function(event) {
 	console.log( 'Initing...' );
 
@@ -7,4 +29,7 @@ document.addEventListener( "DOMContentLoaded", function(event) {
 	document.body.append(div);
 
 	console.log( 'Done!' );
+
+	const domContainer = document.querySelector('#like_button_container');
+	ReactDOM.render(e(LikeButton), domContainer);
 });
