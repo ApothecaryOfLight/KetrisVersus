9b1c9b0ddcf46ad9562de8e3acd5df2ffaba36ef
Ubuntu <ChangerOfWays@gmail.com>
2020-11-15 23:48:28 +0000

    Added login interface.

--- backend/main.js
+++ backend/main.js
@@ -16,20 +16,72 @@ wsServer = new WebSocketServer({
 
 const clients = [];
 
+function doesUserExist( inUsername ) {
+	clients.forEach( client => {
+		if( client.username === inUsername ) {
+			return true;
+		}
+	});
+	return false;
+}
+
 wsServer.on('request', function(request) {
 	//console.dir( request );
 	var connection = request.accept( null, request.origin );
 	console.log( "Connection!" );
-	clients.push( connection );
+	const new_user = {
+		conn : connection,
+		username : "unlogged",
+		password : "unlogged",
+		isLogged : false
+	}
+	//clients.push( new_user );
 	connection.on('message', function( message ) {
 		console.log( "Recieved message!" );
-		console.log( message );
+		const inMessage = JSON.parse( message.utf8Data );
+		console.dir( inMessage );
+		if( inMessage.event === "login" ) {
+			console.log( "Attempting login!" );
+			if( doesUserExist( inMessage.username ) == false ) {
+				new_user.username = inMessage.username;
+				new_user.password = inMessage.password;
+				console.log( "Login approved!" );
+				clients.push( new_user );
+				connection.sendUTF( "login_approved" );
+				//sendNewUserNotification( new_user.username );
+			} else {
+				connection.sendUTF( "login_rejected" );
+			}
+		} else if( inMessage.event === "chat_message" ) {
+			clients.forEach( client => {
+				client.conn.sendUTF( inMessage.text );
+			});
+		} else if( inMessage.event === "game_start" ) {
+
+		} else if( inMessage.event === "game_select" ) {
+
+		} else {
+			console.log( "Unrecognized object!" );
+			console.dir( inMessage );
+		}
+
+		/*console.log( message );
 		clients.forEach( client =>
-			client.sendUTF( message.utf8Data )
-		);
+			client.conn.sendUTF( message.utf8Data )
+		);*/
 	});
 	connection.on( 'close', function( reasonCode, desc ) {
 		console.log( "Closed connection!" );
+		clients.forEach( (client, index) => {
+			if( client.conn === connection ) {
+				console.log( "Loggin out user" );
+				if( client.username != "unlogged" ) {
+					console.log( "Logged out username: " + client.username );
+					//sendLogoutUserNotification( client.username );
+				}
+				clients.splice( index, 1 );
+			}
+		});
 	});
 });
 
--- frontend/content/script.js
+++ frontend/content/script.js
@@ -20,25 +20,15 @@ function launchLoginInterface( inWebsocket ) {
 
 }
 
-function launchChat( inWebsocket ) {
-
-}
-
-document.addEventListener( "DOMContentLoaded", function(event) {
-	console.log( 'Initing...' );
-
-/*	let login_interface = document.getElementById('login_interface');
+function launchChatInterface( ws ) {
+	console.log( "Loggin in..." );
+	let login_interface = document.getElementById('login_interface');
 	let chat_interface = document.getElementById('chat_interface');
-	chat_interface.display = "none";*/
+	login_interface.style.display = "none";
+	chat_interface.style.display = "flex";
 
-	var ws = new WebSocket( 'ws://34.222.250.86:3000' );
-	ws.addEventListener( 'open', function(event) {
-		console.log( "WebSocket opened!" );
-		//ws.onopen = function() { console.log( "Opened!" ); }
-		//ws.onerror = function(error) { console.log( "Error!" ); }
-		//ws.send( 'Hellos!' );
-	});
 	let chatLog = [];
+	//ws.removeEventListener( 'message' );
 	ws.addEventListener( 'message', function(event) {
 		console.log( event.data );
 		chatLog.push( {
@@ -55,6 +45,14 @@ document.addEventListener( "DOMContentLoaded", function(event) {
 		myChatbox.scrollTop =  myChatbox.scrollHeight;
 	});
 
+	function send_chat_message( inMessage ) {
+		const send_message = {
+			event : "chat_message",
+			text : inMessage
+		}
+		const send_message_json = JSON.stringify( send_message );
+		ws.send( send_message_json );
+	}
 
 	let mySendButton = document.getElementById("send_button");
 	mySendButton.addEventListener( 'click', doSend );
@@ -67,7 +65,7 @@ document.addEventListener( "DOMContentLoaded", function(event) {
 			console.log( "enter!" );
 			const textfield = myInputText.value;
 			if( textfield != "" ) {
-				ws.send( textfield );
+				send_chat_message( textfield );
 			}
 			myInputText.value = "";
 		}
@@ -77,7 +75,7 @@ document.addEventListener( "DOMContentLoaded", function(event) {
 		//const input_textfield = document.getElementById("input_text");
 		const input_text = myInputText.value;
 		if( input_text != "" ) {
-			ws.send( input_text );
+			send_chat_message( input_text );
 		}
 		myInputText.value = "";
 		myInputText.focus()
@@ -95,4 +93,52 @@ document.addEventListener( "DOMContentLoaded", function(event) {
 		<ChatRoom chatLog={chatLog} />,
 		document.getElementById('chat_box')
 	);
+}
+
+function doLogin( websocket, username, password ) {
+	const login = {
+		"event" : "login",
+		"username" : username,
+		"password" : password
+	}
+	const login_text = JSON.stringify( login );
+	websocket.send( login_text );
+}
+
+document.addEventListener( "DOMContentLoaded", function(event) {
+	console.log( 'Initing...' );
+
+	let login_interface = document.getElementById('login_interface');
+	let chat_interface = document.getElementById('chat_interface');
+	chat_interface.style.display = "none";
+
+	var ws = new WebSocket( 'ws://34.222.250.86:3000' );
+	ws.addEventListener( 'open', function(event) {
+		console.log( "WebSocket opened!" );
+		//ws.onopen = function() { console.log( "Opened!" ); }
+		//ws.onerror = function(error) { console.log( "Error!" ); }
+		//ws.send( 'Hellos!' );
+	});
+
+	let login_button = document.getElementById('login_button');
+	login_button.addEventListener( 'click', function() {
+		let username_box = document.getElementById('login_username');
+		let password_box = document.getElementById('login_password');
+		let username = username_box.value;
+		let password = password_box.value;
+		if( username != "" && password != "" ) {
+			console.log( "Attempting login!" );
+			doLogin( ws, username, password );
+		}
+	});
+
+	ws.addEventListener( 'message', function(event) {
+		console.log( "Message!" );
+		if( event.data === "login_approved" ) {
+			console.log( "Login approved!" );
+			launchChatInterface( ws );
+		}
+	});
+
+
 });
