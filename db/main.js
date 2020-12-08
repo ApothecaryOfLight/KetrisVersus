5a5f4a030c6a7ed4865be9afd9323684b1abb8c2
Ubuntu <ubuntu@ip-172-31-0-235.us-west-2.compute.internal>
2020-12-08 19:39:29 +0000

    Added run.sh to database client. Began inter-node communication code.

--- db/main.js
+++ db/main.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+const wsServer = require('websocket').server;
+const http = require('http');
 
 const mysqlConnection = mysql.createConnection({
   host: 'localhost',
@@ -6,15 +8,6 @@ const mysqlConnection = mysql.createConnection({
   database: 'ketris_db',
   password: 'ketris_node_user_password'
 });
-
-/*mysqlConnection.query(
-  'select * from test_table;',
-  function( error, results, fields ) {
-    if( error ) { console.log( error ); }
-    console.log( results );
-    //console.log( fields );
-  }
-);*/
 
 function attempt_login ( inUsername, inPassword ) {
   mysqlConnection.query(
@@ -46,30 +39,6 @@ function does_username_exist( inUsername ) {
 }
 
 function create_user ( inUsername, inPassword ) {
-/*  mysqlConnection.query(
-    'SELECT * FROM ketris_users WHERE username_hash=UNHEX(MD5(\"'+inUsername+'\"))',
-    function( error, results, fields ) {
-      if( error ) { console.log( error ); }
-      //console.log( results.length );
-      if( results.length == 0 ) {
-        console.log( "Username available! Creating user..." );
-        mysqlConnection.query(
-          'INSERT INTO ketris_users ' +
-          '(username_hash,password_hash,username_plaintext,account_creation) VALUES (' +
-          'UNHEX(MD5(\"' + inUsername + '\")), ' +
-          'UNHEX(MD5(\"' + inPassword + '\")), ' +
-          '\"' + inUsername + '\", ' +
-          '\'1999-01-01 01:01:01\' );',
-          function( error, results, fields ) {
-            if( error ) { console.log( error ); }
-            console.log( "User created!" );
-          }
-        );
-      } else {
-        console.log( "User exists!" );
-      }
-    }
-  )*/
   if( does_username_exist( inUsername ) == false ) {
     console.log( "Username available! Creating user..." );
     mysqlConnection.query(
@@ -85,9 +54,6 @@ function create_user ( inUsername, inPassword ) {
       }
     );
   }
-  /*mysqlConnection.query(
-    'INSERT INTO ketris_users
-  );*/
 }
 
 function log_match( inPostingUser, inAcceptingUser, inStartTime, inEndTime,
@@ -119,9 +85,38 @@ function log_message_to_dev( inAuthor, inBodyText, inPostingTime ) {
   );
 }
 
+function retrieve_dev_messages() {
+  mysqlConnection.query(
+    'SELECT * FROM ketris_messages;',
+    function( error, results, fields ) {
+      if( error ) { console.log( error ); return error; }
+      return result;
+    }
+  );
+}
+
 //create_user( "bob", "bobspassword" );
 //create_user( "sally", "sallyspassword" );
-attempt_login( "bob", "bobspassword" );
-log_match( "bob", "sally", "1999-01-01 01:01:01", "1999-02-02 01:01:01", 90, 10 );
-log_match( "sally", "bob", "1998-01-01 01:01:01", "1998-02-02 01:01:01", 85, 20 );
-log_message_to_dev( "Good Employer", "We will hire you!", "2020-12-15 10:00:00" );
+//attempt_login( "bob", "bobspassword" );
+//log_match( "bob", "sally", "1999-01-01 01:01:01", "1999-02-02 01:01:01", 90, 10 );
+//log_match( "sally", "bob", "1998-01-01 01:01:01", "1998-02-02 01:01:01", 85, 20 );
+//log_message_to_dev( "Good Employer", "We will hire you!", "2020-12-15 10:00:00" );
+
+const myServer = http.createServer( function( request, response ) {} );
+myServer.listen( 8989, function() {
+  console.log( "Now listening on 8989" );
+});
+const myWSServer = new wsServer({
+  httpServer: server
+});
+
+myWSServer.on('request', function( request ) {
+  console.log( "Connection!" );
+  var ketris_backend = request.accept( null, request.origin );
+  ketris_backend.on( 'message', function( message ) {
+    const inMessage = JSON.parse( message.utf8Data );
+    console.log( "Recieved message." );
+    console.dir( inMessage );
+    ketris_backend.send( 'yepuruni' );
+  });
+});
