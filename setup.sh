53ee3deb9695e6b905ef83b3af6098aa2a1a09b6
Ubuntu <ubuntu@ip-172-31-17-66.us-west-2.compute.internal>
2021-06-18 20:17:22 +0000

    Added missing sudo to IPTables command.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 #Install NodeJS
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install nodejs -y
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo ufw allow 22
 sudo ufw allow 80
 sudo ufw allow 8080
