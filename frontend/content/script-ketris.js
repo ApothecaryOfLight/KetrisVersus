ef41adc8bf4d178d749b7e67eb3a1ccd1bfe8da9
Ubuntu <ChangerOfWays@gmail.com>
2021-07-03 22:09:24 +0000

    Fixed game screen clicks on mobile and desktop.

--- frontend/content/script-ketris.js
+++ frontend/content/script-ketris.js
@@ -1484,24 +1484,44 @@ function launchKetris( inIPAddress, inGameID ) {
   }
   window.onclick = function(e) {
     let out = getCursorPosition( myKetrisCanvas, e );
+    console.log( out[0] + "/" + out[1] );
     if( myGameState.GlobalPlay == true ) {
       if( myGameState.Paused == false ) {
-        if( out[0] < 313 ) {
-          if( out[1] < (620/2) ) {
+        if( isMobile ) {
+          if( out[1] < 305 ) {
             doUpKeyPress();
-          }
-          if( out[1] > (620/2) && out[1] ) {
-            if( out[0] < 310/3 ) {
+          } else if( out[1] > 305  ) {
+            if( out[0] < 65 ) {
               doLeftKeyPress();
             }
             if(
-              out[0] > (310/3) &&
-              out[0] < (310/3)*2
+              out[0] > 65 &&
+              out[0] < 130
             ) {
               doDownKeyPress();
             }
-            if( out[0] > (310/3)*2 ) {
+            if( out[0] > 130 ) {
               doRightKeyPress();
+            }
+          }
+        } else {
+          if( out[0] < 313 ) {
+            if( out[1] < (620/2) ) {
+              doUpKeyPress();
+            }
+            if( out[1] > (620/2) && out[1] ) {
+              if( out[0] < 310/3 ) {
+                doLeftKeyPress();
+              }
+              if(
+                out[0] > (310/3) &&
+                out[0] < (310/3)*2
+              ) {
+                doDownKeyPress();
+              }
+              if( out[0] > (310/3)*2 ) {
+                doRightKeyPress();
+              }
             }
           }
         }
@@ -1509,7 +1529,6 @@ function launchKetris( inIPAddress, inGameID ) {
     }
     if( myGameState.GameOver == true ) {
       console.log( "Click on Restart Game." );
-      console.log( out[0] + "/" + out[1] );
       if( isMobile ) {
         if( out[0] > 42 && out[0] < 149 ) {
           if( out[1] > 188 && out[1] < 220 ) {
