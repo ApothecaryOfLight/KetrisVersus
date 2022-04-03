1c488fe98234eb50d35653b84c1ca56f9aa6d7df
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-03 18:22:19 +0000

    Removed deprecated commands in install script as Nginx has replaced NodeJS for content.

--- install.sh
+++ install.sh
@@ -16,8 +16,6 @@ then
   #Open ports.
   sudo ufw allow 22
   sudo ufw allow 80
-  sudo ufw allow 8080
-  sudo ufw allow 8081
   sudo ufw allow 3002 #login/chat port.
   sudo ufw allow 3003 #ketris game port.
   sudo ufw enable
@@ -31,10 +29,6 @@ then
   sudo mysql -s < ketris_sql_init.sql
   rm ketris_sql_init.sql
 
-  #Add prerouting.
-  ##sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8081
-  ##sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
-
   echo "KetrisVS standalone installation complete!";
 elif [[ "$1" = "unified" ]];
 then
@@ -42,12 +36,9 @@ then
 
   #Install NodeJS packages.
   cd chat-backend && npm i
-  cd ../frontend && npm i
-  cd ../ketris-backend && npm i
+  cd ketris-backend && npm i
 
   #Open ports.
-  sudo ufw allow 8080
-  sudo ufw allow 8081
   sudo ufw allow 3002 #login/chat port.
   sudo ufw allow 3003 #ketris game port.
 
@@ -55,10 +46,6 @@ then
   echo -e "CREATE DATABASE ketris_db;\nCREATE USER 'ketris_node_user'@'localhost' IDENTIFIED BY 'ketris_node_user_password';\nGRANT ALL ON ketris_db.* TO 'ketris_node_user'@'localhost';\nUSE ketris_db;\nCREATE TABLE ketris_users ( username_hash BINARY(16) NOT NULL, password_hash BINARY(16) NOT NULL, username_plaintext VARCHAR(256) NOT NULL, account_creation DATETIME NOT NULL, last_login DATETIME, PRIMARY KEY( username_hash ) );\nCREATE TABLE ketris_matches ( posting_user BINARY(16) NOT NULL, accepting_user BINARY(16) NOT NULL, timestamp_start DATETIME NOT NULL, timestamp_end DATETIME NOT NULL, posting_user_score INT NOT NULL, accepting_user_score INT NOT NULL );\nCREATE TABLE ketris_messages ( author_name VARCHAR( 256 ) NOT NULL, message_body MEDIUMTEXT NOT NULL, timestamp DATETIME NOT NULL );" > ketris_sql_init.sql
   sudo mysql -s < ketris_sql_init.sql
   rm ketris_sql_init.sql
-
-  #Add prerouting.
-  ##sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8081
-  ##sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
 
   echo "KetrisVS unified installation complete!"
 else
