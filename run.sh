b60a5241549c17d0c0f9c33dd33509ef42054ecf
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-03 18:16:41 +0000

    Removed script to run frontend as it is now served by Nginx. Fixed border location in CSs.

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ then
   IP=$(hostname -I | xargs)
   echo "const ip = \"ws://${2}\";" > ./frontend/content/ip_file.js
   screen -d -m -S ketris_chat bash -c 'cd chat-backend && ./run.sh dev'
-  screen -d -m -S ketris_frontend bash -c 'cd frontend && ./run.sh dev'
+##  screen -d -m -S ketris_frontend bash -c 'cd frontend && ./run.sh dev'
   screen -d -m -S ketris_backend bash -c 'cd ketris-backend && ./run.sh dev'
 elif [[ "$1" = "https" ]];
 then
   echo "const ip = \"wss://ketris.net\";" > ./frontend/content/ip_file.js
   screen -d -m -S ketris_chat bash -c 'cd chat-backend && ./run.sh prod'
-  screen -d -m -S ketris_frontend bash -c 'cd frontend && ./run.sh prod'
+##  screen -d -m -S ketris_frontend bash -c 'cd frontend && ./run.sh prod'
   screen -d -m -S ketris_backend bash -c 'cd ketris-backend && ./run.sh prod'
 else
   echo "Command line argument:";
