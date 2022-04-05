c04dd2d0517cb3f56fe57fd60d1e622fc7af70fd
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-05 20:15:46 +0000

    Seperated mobile CSS into seperate files.

--- frontend/ketris-javascript/game-javascript/launch-ketris.js
+++ frontend/ketris-javascript/game-javascript/launch-ketris.js
@@ -2,7 +2,6 @@ function launchKetris( inIPAddress, inGameID ) {
     console.log( "Connection to server " + inIPAddress + ":3003 for game " + inGameID + "." );
     console.log( "isMobile: " + isMobile );
     console.log( "Launching Ketris." );
-    
+
     doLaunchWebsocket( inIPAddress, inGameID );
-  }
-  
+}
