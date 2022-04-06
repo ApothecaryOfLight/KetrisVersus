16867cba073fd85ea792f4169a9301826249c02d
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-06 19:59:50 +0000

    Corrected HTTP/HTTPS generated in ip_getter.js.

--- frontend/ketris-javascript/admin-javascript/ip_getter.js
+++ frontend/ketris-javascript/admin-javascript/ip_getter.js
@@ -1,5 +1,5 @@
 function get_ip() {
-    if( ip.substr(0,2) == "ws" ) {
+    if( ip.substr(0,3) == "ws:" ) {
         return "http://" + ip.substr(5, ip.length) + ":53004/";
     } else if( ip.substr(0,3) == "wss" ) {
         return "https://" + ip.substr(5, ip.length) + ":53004/";
