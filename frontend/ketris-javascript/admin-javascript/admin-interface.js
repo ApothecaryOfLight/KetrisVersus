5068096ca48bca7c160b9c37a86ac6aec04f3d7a
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-11 23:32:11 +0000

    Added buttons in admin to switch between error and event logs.

--- frontend/ketris-javascript/admin-javascript/admin-interface.js
+++ frontend/ketris-javascript/admin-javascript/admin-interface.js
@@ -3,6 +3,20 @@
 window.onload = () => {
     console.log( "Loaded!" );
 
-    get_error_log();
-    get_event_log();
+    const get_error_log_button = document.getElementById("menu_button_error_log");
+    get_error_log_button.addEventListener( "click", (event) => {
+        const error_log_container = document.getElementById("error-log-container");
+        error_log_container.style["display"] = "block";
+        const event_log_container = document.getElementById("event-log-container");
+        event_log_container.style["display"] = "none";
+        get_error_log();
+    });
+    const get_event_log_button = document.getElementById("menu_button_event_log");
+    get_event_log_button.addEventListener( "click", (event) => {
+        const error_log_container = document.getElementById("error-log-container");
+        error_log_container.style["display"] = "none";
+        const event_log_container = document.getElementById("event-log-container");
+        event_log_container.style["display"] = "block";
+        get_event_log();
+    });
 }
