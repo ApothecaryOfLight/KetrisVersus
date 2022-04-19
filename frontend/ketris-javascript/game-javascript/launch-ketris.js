81d1bbf8629c0cdb7af37ef11303928fbee89d7c
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-19 16:41:56 +0000

    Added additional comments to various Keris game JavaScript files.

--- frontend/ketris-javascript/game-javascript/launch-ketris.js
+++ frontend/ketris-javascript/game-javascript/launch-ketris.js
@@ -2,9 +2,11 @@
 Launch the Ketris game.
 */
 function launchKetris( inIPAddress, inGameID ) {
+    //Print some standard information about the game to the console for development purposes.
     console.log( "Connection to server " + inIPAddress + ":3003 for game " + inGameID + "." );
     console.log( "isMobile: " + isMobile );
     console.log( "Launching Ketris." );
 
+    //Launc the Websocket connection to the game server.
     doLaunchWebsocket( inIPAddress, inGameID );
 }
--- frontend/ketris-javascript/game-javascript/launch_ketris_game.js
+++ frontend/ketris-javascript/game-javascript/launch_ketris_game.js
@@ -3,19 +3,32 @@ Launch the Ketris game itself.
 */
 function doLaunchKetrisGameplayer() {
   console.log( "Launching Ketris Gameplayer." );
+
+  //Attach event listeners to the mouse.
   attach_game_mouse_events();
+
+  //Reset the global variables to default values.
   format_globals();
+
+  //Set the canvas width to the default value.
   let playCanvasWidth = 313;
   if( isMobile == false ) {
+    //If we're on desktop, double the canvas width to hold enemy play area as well.
     playCanvasWidth = 313*2;
   } else {
+    //Otherwise, if we're on mobile, set the canvas width to the default, half value.
     const myKetrisCanvas = document.getElementById("myKetrisCanvas");
     const myKetrisCanvasContext = myKetrisCanvas.getContext("2d");
     myKetrisCanvasContext.canvas.width = 313;
   }
+
+  //Request the sprite atlas from the server.
   myDOMHandles.KetrisImage.src = "ketris_media/spritesheet_mod.png";
+
+  //Attach an event listener to await the completion of loading the spite atlas.
   myDOMHandles.KetrisImage.onload = function() {
     console.log( "Images loaded.");
+    //Set the dimensions of each canvas.
     myDOMHandles.myBackgroundCanvas = document.createElement("canvas");
     myDOMHandles.myBackgroundCanvas.width = playCanvasWidth;
     myDOMHandles.myBackgroundCanvas.height = 749;
@@ -44,9 +57,13 @@ function doLaunchKetrisGameplayer() {
     myDOMHandles.myEnemyCanvas.width = 313;
     myDOMHandles.myEnemyCanvas.height = 749;
 
+    //Buffer the background, so we don't have to rebuild it every draw.
     doComposeBackground();
+
+    //Start the new Ketris game.
     doStartNewGame();
 
+    //Begin the animation request sequence.
     myAnimationValues.AnimationFrameHandle =
       window.requestAnimationFrame( doManageDrawing );
   };
@@ -57,11 +74,21 @@ function doLaunchKetrisGameplayer() {
 End the Ketris game.
 */
 function doEndKetrisGameplayer() {
+  //Remove the mouse related event listeners.
   detach_game_mouse_events();
+
+  //Stop the animation request sequence.
   cancelAnimationFrame( myAnimationValues.AnimationFrameHandle );
+
+  //Remove the event listener for document visibility change (going
+  //to other tabs, for example).
   document.removeEventListener( 'visibilitychange', on_visibility_change );
+
+  //Get references to the game and chat interfaces.
   let game_interface = document.getElementById('game_interface');
   let chat_interface = document.getElementById('chat_interface');
+
+  //Hide the game interface, show the chat interface.
   game_interface.style.display = "none";
   chat_interface.style.display = "flex";
 }
